f9f015e6274afa1abd7eb957e0dcf32f8ac21b08
Daniel Quintanilla <daniel.quintanilla@shopgate.com>
2019-03-26 14:56:44 -0500

    Updating for unit tests

---
+++ frontend/portals/ProductReviews/index.spec.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { mount } from 'enzyme';
+import mockRenderOptions from '@shopgate/pwa-common/helpers/mocks/mockRenderOptions';
+import { basicProductState } from '@shopgate/pwa-common-commerce/product/mock';
+import { RouteContext } from '@shopgate/pwa-common/context';
+import ProductReviews from './index';
+
+const mockedStore = configureStore();
+jest.mock('../../components/HtmlReferenceContainer', () => () => (<div>HtmlReferenceContainer</div>));
+
+/**
+ * Create Component
+ * @return {ReactWrapper}
+ */
+const createComponent = () => mount(
+  <RouteContext.Provider value={{ params: { product: 'product_one' } }}>
+    <Provider store={mockedStore({ product: basicProductState })}>
+      <ProductReviews />
+    </Provider>
+  </RouteContext.Provider>,
+  mockRenderOptions
+);
+
+describe('ProductReviews', () => {
+  it('should render', () => {
+    const component = createComponent();
+    expect(component).toMatchSnapshot();
+  });
+});
